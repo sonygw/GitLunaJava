50768316d25d306173e068e6e7cce7fe31354c1a
Lou <oryxpyro@live.fr>
2017-11-29 15:07:12 +0100

    DAO ArtCom + interface.

--- src/main/java/com/formation/DAO/ArtComDAO.java
+++ src/main/java/com/formation/DAO/ArtComDAO.java
@@ -1,5 +1,16 @@
 package com.formation.DAO;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.formation.model.ArtCom;
+
 public interface ArtComDAO {
 
+	public ArrayList<ArtCom> SelectAllArticleFromCommande(int idCommande);
+	public boolean DeleteArticleFromCommande(ArtCom obj);
+	public boolean UpdateArticleFromCommande(ArtCom obj, int idArtCom);
+	public boolean CreateArticleFromCommande(ArtCom obj);
+	
+	
 }
