6a91ac76ce82833015fb11a7301ca37989440664
Lou <oryxpyro@live.fr>
2017-11-29 13:25:04 +0100

    Ajout de la DAO User interface + implémentation

--- src/main/java/com/formation/DAO/UserDAO.java
+++ src/main/java/com/formation/DAO/UserDAO.java
@@ -1,5 +1,16 @@
 package com.formation.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import com.formation.model.User;
+
 public interface UserDAO {
 
+	public User SelectUser(Connection con, int id);
+	public ResultSet SelectAllUsers(Connection con);
+	public boolean UpdateUser(Connection con, int id);
+	public boolean CreateUser(Connection con);
+	public boolean DeleteUser(Connection con, int id);
+	
 }
--- src/main/java/com/formation/DAO/UserDAOSQL.java
+++ src/main/java/com/formation/DAO/UserDAOSQL.java
@@ -1,5 +1,40 @@
 package com.formation.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import com.formation.model.User;
+
 public class UserDAOSQL implements UserDAO {
+
+	@Override
+	public User SelectUser(Connection con, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultSet SelectAllUsers(Connection con) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean UpdateUser(Connection con, int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean CreateUser(Connection con) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean DeleteUser(Connection con, int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
