70cf60d3381409730249610ae8be66d6752e84c1
Daniel <arenas.daniel14@gmail.com>
2017-12-05 16:39:07 +0100

    Ctrl Maj F

--- src/main/java/com/formation/DAO/ClientDAO.java
+++ src/main/java/com/formation/DAO/ClientDAO.java
@@ -8,50 +8,61 @@ public interface ClientDAO {
 
 	/**
 	 * Sélection d'un client en fonction de son id.
-	 * @param id du client
+	 * 
+	 * @param id
+	 *            du client
 	 * @return un client suivant son id
 	 */
 	public Client SelectClient(int id);
 
 	/**
 	 * Sélection de tous les clients de la base de données sous forme d'ArrayList.
+	 * 
 	 * @return Arraylist de tous les clients.
 	 */
 	public ArrayList<Client> SelectAllClients();
+
 	/**
-	 *  Sélection de tous les clients de la base de données avec tri par nom. 
-	 * @param nom du client
+	 * Sélection de tous les clients de la base de données avec tri par nom.
+	 * 
+	 * @param nom
+	 *            du client
 	 * @return Arraylist de tous les clients triés par leur nom.
 	 */
 	public ArrayList<Client> SelectAllClientsByName(String nom);
-	
+
 	/**
 	 * Suppression d'un objet Client.
-	 * @param obj client
+	 * 
+	 * @param obj
+	 *            client
 	 * @return booléen de vérification : vrai si réussi
 	 */
 	public boolean DeleteClient(Client obj);
-	
 
 	/**
 	 * Modification d'un objet Client dans la base de données.
-	 * @param obj client
-	 * @param id du client
+	 * 
+	 * @param obj
+	 *            client
+	 * @param id
+	 *            du client
 	 * @return booléen de vérification : vrai si réussi
 	 */
 	public boolean UpdateClient(Client obj, int id);
-	
 
 	/**
 	 * Création d'un objet client dans la base de données.
-	 * @param obj client
+	 * 
+	 * @param obj
+	 *            client
 	 * @return booléen de vérification : vrai si reussi
 	 */
 	public boolean CreateClient(Client obj);
-	
-	
+
 	/**
 	 * Sélection du dernier client de la base de données.
+	 * 
 	 * @return le dernier client de la base de données
 	 */
 	public Client SelectLastClient();
