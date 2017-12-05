4fff885b2bdb4214503c2a2e61a6846421a2f2d7
Lou <oryxpyro@live.fr>
2017-12-05 14:57:47 +0100

    Regex CP + Numéro tel

--- src/main/java/com/formation/main/Regex.java
+++ src/main/java/com/formation/main/Regex.java
@@ -4,13 +4,38 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Regex {
-	public static void testCP(String CP) {
-		String chaine = "1a1b1e2d";
+	public static boolean testCP(String CP) {
+		String chaine = CP;
+		boolean verif = false;
+		if (chaine.length() != 5 )
+		{verif = true;
+			return verif;
+		}
 		Pattern p = Pattern.compile("[a-zA-Z]");
 		Matcher m = p.matcher(chaine);
 		while(m.find()) {
-			System.out.println(m.group());
+			verif =true;
 			
 		}
+		
+		return verif;
+	}
+	
+	
+	public static boolean testNum(String num) {
+		String chaine = num;
+		boolean verif = false;
+		if (chaine.length() != 10 )
+		{verif = true;
+			return verif;
+		}
+		Pattern p = Pattern.compile("[a-zA-Z]");
+		Matcher m = p.matcher(chaine);
+		while(m.find()) {
+			verif =true;
+			
+		}
+		
+		return verif;
 	}
 }
